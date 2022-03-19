b6b6268af56ea7b346e90e810e5198d08a6ac677
Jacob Phillips <jacobphillips@outlook.dk>
2022-03-19 21:41:18 +0100

    AI update

---
+++ Projects/Artificial Intelligence & Search Algorithms/A-star search function/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def a_star_search(problem, inital_state, goal, ):
+    if inital_state == goal:
+        return None
+
+    return None
+
+
+if __name__ == '__main__':
+    print(f'Hello World')
